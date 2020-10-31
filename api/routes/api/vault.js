53f8ad98d364ddc8ceac53ac258b8157fd5af3cc
Roman Padilla <monyrp.apps@gmail.com>
2020-10-31 02:46:11 -0600

    Added alert when user saves duplicate record (EARLY).

--- api/routes/api/vault.js
+++ api/routes/api/vault.js
@@ -1,56 +1,70 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const Vault = require("../../modules/vault");
+const Vault = require('../../modules/vault');
 
 // route - GET api/vault/get-credentials
 // description -
 // access - Public
-router.get("/get-credentials/:sitename", async (req, res) => {
+router.get('/get-credentials/:sitename', async (req, res) => {
   try {
-    console.log("entered /api/vault/get-credentials");
+    console.log('entered /api/vault/get-credentials');
     const credentials = await Vault.find({ sitename: req.params.sitename });
 
     res.json(credentials);
   } catch (error) {
-    console.log("/get-credentials: " + error);
+    console.log('/get-credentials: ' + error);
   }
 });
 
 // route - GET api/vault/load-names
 // description -
 // access - Public
-router.get("/load-names", async (req, res) => {
+router.get('/load-names', async (req, res) => {
   try {
-    console.log("entered /api/vault/load-names");
-    const credentials = await Vault.find();
+    let siteNames = new Set();
+    const credentials = await Vault.find({}, 'sitename');
 
-    res.json(credentials);
+    for (let i = 0; i < credentials.length; i++) {
+      siteNames.add(credentials[i].sitename);
+    }
+
+    res.json(Array.from(siteNames));
   } catch (error) {
-    console.log("/load-names: " + error);
+    console.log('/load-names: ' + error);
   }
 });
 
 // route - POST api/vault/save-credentials
 // description -
 // access - Public
-router.post("/save-credentials", (req, res) => {
+router.post('/save-credentials', (req, res) => {
   try {
-    let vaultEntry = new Vault({
-      sitename: req.body.sitename,
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-      comment: req.body.comment,
-    });
+    // let search1 = Vault.findOne({ sitename: req.body.sitename, username: req.body.username });
 
-    vaultEntry.save();
+    // let search2 = Vault.findOne({ sitename: req.body.sitename, email: req.body.email });
 
-    res.status(200);
+    // if (!(search1 || search2)) {
+    //   let vaultEntry = new Vault({
+    //     sitename: req.body.sitename,
+    //     username: req.body.username,
+    //     email: req.body.email,
+    //     password: req.body.password,
+    //     comment: req.body.comment,
+    //   });
+
+    //   vaultEntry.save();
+
+    //   res.status(200);
+    // } else {
+      console.log("sending 200;");
+      res.status(400).send('Server Error');
+      // res.status(200);
+    // }
     //   console.log("req.boby.sitename: " + req.body.sitename);
     //   res.send(req.params);
   } catch (error) {
-    console.log("/save-credentials: " + error);
+    console.log('/save-credentials: ' + error);
   }
 });
 
